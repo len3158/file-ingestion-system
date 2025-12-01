6a006314128e5c083cb3bf4aacf53970708056c7
Lenny Siemeni <len3158@gmail.com>
2025-12-01 18:01:59 -0500

    Initial commit

---
+++ src/main/java/com/example/fileingestionsystem/FileIngestionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileingestionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIngestionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileIngestionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fileingestionsystem/FileIngestionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileingestionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileIngestionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
